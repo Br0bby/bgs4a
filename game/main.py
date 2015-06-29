3dce0f8a0b7c0e5807d432ce85f3b7504adf2783
Br0bby <gillimonster.dev@gmail.com>
2015-06-29 19:41:27 +0200

    audio and surface buffer

--- game/main.py
+++ game/main.py
@@ -1,19 +1,78 @@
+import time
 from pygame import *
 
+
+
 def main():
+    #t0 = time.time()
+    #tindex += 1
     screen.fill("green")
-    screen.blit(background, (0, 0))
-    screen.blit(image, (415, 311)) # 1280 / 2 (screen width) - 450 / 2 (image width)...
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0)) # 10: 60+ FPS
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0)) # 20: 32 FPS
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0)) # 30: 25FPS
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0)) # 40: 21 FPS
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0))
+    screen.blit(image, (0, 0)) # 50: 17 FPS
+
+    #if tindex % 10 == 1:
+    #    print(1 / (tt / tindex), "FPS")
+    #tt += (time.time() - t0)
 
 if __name__ == "__main__":
+    #t0 = time.time()
+    #tt = 0
+    #tindex = 0
     pygame.display.set_icon("http://www.stackoverflow.com/favicon.ico")
     pygame.display.set_caption("Pygame Title")
 
-    #pygame.mixer.music.load("assets/music/battle.ogg")
-    #pygame.mixer.music.play(-1)
+    pygame.mixer.music.load("assets/music/final_boss.ogg")
+    pygame.mixer.music.play(-1)
 
-    screen = pygame.display.set_mode((1280, 800), 1) # fullscreen flag
-    background = pygame.image.load("assets/img/background.jpg") # 1280x800
+    screen = pygame.display.set_mode((800, 480), 1) # fullscreen flag
     image = pygame.image.load("assets/img/logo.png") # 450x178
 
     pygame.loop(main, 32) # <--- 32 = frames per second
